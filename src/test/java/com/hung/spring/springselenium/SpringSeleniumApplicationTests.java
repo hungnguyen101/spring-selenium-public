05f2b45ca35b79154f2dbcdb09f59ead8273cb48
Hung QA <nguyenthehung93sgu@gmail.com>
2022-08-05 14:59:46 +0700

    update result directory for test result

---
+++ src/test/java/com/hung/spring/springselenium/SpringSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hung.spring.springselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
